2002b2aef658ede19a361440cc965b057bec826d
greeshma017 <gdharmapalan00@mylangara.ca>
2024-02-18 12:12:55 -0800

    feat: refine folder stucture

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import './style.css'
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App'
-
-// const root = ReactDOM.createRoot(document.getElementById('react-container'));
-
-// root.render(
-//     <React.StrictMode>
-//         <App title="Hello React App with props!" subtitle="My first React app is here!!!" />
-//     </React.StrictMode>
-// );
+import App from './components/App'
 
 ReactDOM.render(
-    <App title="Welcome to Lumiere!" subtitle="My first React app is here at Netlify!!!" />,
-    document.getElementById('react-container')
+    <App />,
+    document.getElementById('root')
 );

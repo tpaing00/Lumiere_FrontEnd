7957086a5bebf61b673f9d64972628ecf72fe3c9
greeshma017 <gdharmapalan00@mylangara.ca>
2024-02-23 18:54:40 -0800

    feat: P2S-192 update on isLogged hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './style.css'
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
 import App from './components/App'
 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );

02747229219fee5cf1596c392a9da0ea7abca688
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-03-01 15:56:57 -0800

    Creates inventory file

---
+++ src/components/Inventory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductList from './ProductList';
+
+const Inventory = () => {
+
+    return (
+        <>
+        <ProductList />
+        </>
+    )
+
+}
+
+export default Inventory

e8646d7daf6cee76ad8c1c82a5b3d798ac5b23ca
tpaing00 <tpaing00@mylangara.ca>
2024-03-05 16:06:16 -0800

    feat: P2S-231- Fix AddProduct, Scanner, ScannerDetail, StaffCheckout, RetailCheckout and read from predefine json for some dropdown fields

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,143 +1,253 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import inventoryTypeData from "./predefined_data/inventorytype.json";
+import productCategoryData from "./predefined_data/productcategory.json";
 
 const ProductList = () => {
+  // data from db:
+  const navigate = useNavigate();
+  const [inventoryData, setInventoryData] = useState([]);
+  const [productData, setProductData] = useState([]);
+  const [notificationData, setNotificationData] = useState([]);
 
-    // data from db:
-    const [inventoryData, setInventoryData] = useState([]);
-    const [productData, setProductData] = useState([]);
+  // sorting and filtering:
+  const [filterByInventory, setFilterByInventory] = useState("");
+  const [filterByCategory, setFilterByCategory] = useState("");
+  const [sortByBrand, setSortByBrand] = useState("");
 
-    // sorting and filtering:
-    const [filterByInventory, setFilterByInventory] = useState("");
-    const [filterByCategory, setFilterByCategory] = useState("");
-    const [sortByBrand, setSortByBrand] = useState("");
+  useEffect(() => {
+    Promise.all([
+      axios.get("https://api.lumiereapp.ca/api/v1/inventory"),
+      axios.get("https://api.lumiereapp.ca/api/v1/products"),
+      axios.get("https://api.lumiereapp.ca/api/v1/notification"),
+    ])
+      .then((responses) => {
+        const inventoryResponse = responses[0].data;
+        const productsResponse = responses[1].data;
+        const notificationsResponse = responses[2].data;
+        setInventoryData(inventoryResponse);
+        setProductData(productsResponse);
+        setNotificationData(notificationsResponse);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        Promise.all([
-            axios.get('https://api.lumiereapp.ca/api/v1/inventory'),
-            axios.get('https://api.lumiereapp.ca/api/v1/products')
-        ])
-            .then(responses => {
-                const inventoryResponse = responses[0].data;
-                const productsResponse = responses[1].data;
-                setInventoryData(inventoryResponse);
-                setProductData(productsResponse);
+  // console.log(inventoryData)
+  // console.log(productData)
 
+  const renderTableHeader = () => {
+    // Define the keys that you want to display in the table
+    let header = [
+      "productName",
+      "brandName",
+      "category",
+      "dateAdded",
+      "expiryDate",
+      "status",
+      "addToInventory",
+    ];
+    return header.map((key, index) => {
+      return <th key={index}>{key}</th>;
+    });
+  };
 
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
+  const renderTableData = () => {
+    // Check if both inventoryData and productData are available
+    if (
+      inventoryData.length === 0 ||
+      productData.length === 0 ||
+      notificationData.length === 0
+    ) {
+      return null; // Return null if either array is empty
+    }
 
-    // console.log(inventoryData)
-    // console.log(productData)
+    const combinedData = inventoryData.map((inventoryRow) => {
+      // Find the matching product object from the productData array based on the barcodeNumber
+      const matchingProduct = productData.find(
+        (product) => product.barcodeNumber === inventoryRow.barcodeNumber
+      );
 
-    const renderTableHeader = () => {
-        // Define the keys that you want to display in the table
-        let header = ["productName", "brandName", "category", "dateAdded", "expiryDate", "addToInventory"];
-        return header.map((key, index) => {
-            return <th key={index}>{key}</th>
-        });
+      // Merge the properties of inventoryRow and matchingProduct into a new object
+      if (matchingProduct) {
+        const combined = {
+          ...inventoryRow,
+          ...matchingProduct,
+          dateAdded: formatDate(inventoryRow.dateAdded),
+          expiryDate: formatDate(inventoryRow.expiryDate),
+          status: calculateStatus(inventoryRow._id),
+        };
+
+        // Remove the duplicate _id field if it exists
+        delete combined._id;
+        return combined;
+      } else {
+        return inventoryRow;
+      }
+    });
+
+    // Sort the combined data based on the selected option for brandName
+    let sortedData = [...combinedData];
+    // Sort the filtered data based on the selected option for brandName
+    if (sortByBrand === "asc") {
+      sortedData.sort((a, b) => a.brandName.localeCompare(b.brandName)); // Sort by ascending order of brandName using string comparison
+    } else if (sortByBrand === "desc") {
+      sortedData.sort((a, b) => b.brandName.localeCompare(a.brandName)); // Sort by descending order of brandName using string comparison
+    }
+    // Filter the combined data based on the selected options
+    const filteredData = sortedData.filter((row) => {
+      // Check if the row matches the filter criteria for addToInventory and category
+      const inventoryMatch =
+        filterByInventory === "" || row.addToInventory === filterByInventory;
+      const categoryMatch =
+        filterByCategory === "" || row.category === filterByCategory;
+      // Return true if both criteria are met
+      return inventoryMatch && categoryMatch;
+    });
+
+    // Render table rows
+    const renderStatusStyle = (status) => {
+      switch (status) {
+        case "In Stock":
+          return { color: "green" };
+        case "Low Stock":
+          return { color: "orange" };
+        case "Expired":
+          return { color: "red" };
+        case "Out of Stock":
+          return { color: "purple" };
+        default:
+          return {};
+      }
     };
 
-
-    const renderTableData = () => {
-        // Combine the data from both arrays
-        const combinedData = inventoryData.map(row => {
-            // Find the matching object from the other array based on the inventoryId
-            const match = productData.find(product => product.barcodeNumber === row.barcodeNumber);
-            // Merge the matching objects into a new object
-            const combined = Object.assign({}, row, match);
-            // Return the new object
-            return combined;
-        });
-        // Filter the combined data based on the selected options
-        const filteredData = combinedData.filter(row => {
-            // Check if the row matches the filter criteria for addToInventory and category
-            const inventoryMatch = filterByInventory === "" || row.addToInventory === filterByInventory;
-            const categoryMatch = filterByCategory === "" || row.category === filterByCategory;
-            // Return true if both criteria are met
-            return inventoryMatch && categoryMatch;
-        });
-
-        // Sort the filtered data based on the selected option for brandName
-        if (sortByBrand === "asc") {
-            filteredData.sort((a, b) => a.brandName.localeCompare(b.brandName)); // Sort by ascending order of brandName using string comparison
-        } else if (sortByBrand === "desc") {
-            filteredData.sort((a, b) => b.brandName.localeCompare(a.brandName)); // Sort by descending order of brandName using string comparison
+    return filteredData.map((row, index) => {
+      let col = [
+        "productName",
+        "brandName",
+        "category",
+        "dateAdded",
+        "expiryDate",
+        "status",
+        "addToInventory",
+      ].map((key) => {
+        // Apply styling only to the status column
+        if (key === "status") {
+          return (
+            <td key={key} style={renderStatusStyle(row.status)}>
+              {row[key]}
+            </td>
+          );
+        } else {
+          return <td key={key}>{row[key]}</td>;
         }
+      });
+      return <tr key={index}>{col}</tr>;
+    });
+  };
 
-        return filteredData.map((row, index) => {
-            // Get the values of the keys that you want to display in the table
-            let col = ["productName", "brandName", "category", "dateAdded", "expiryDate", "addToInventory"].map(key => row[key]);
-            return (
-                <tr key={index}>
-                    {col.map((val, index) => {
-                        return <td key={index}>{val}</td>
-                    })}
-                </tr>
-            );
-        });
-    };
+  // Handle the change of the dropdown value for addToInventory
+  const handleInventoryChange = (event) => {
+    setFilterByInventory(event.target.value);
+  };
 
-    // Handle the change of the dropdown value for addToInventory
-    const handleInventoryChange = (event) => {
-        setFilterByInventory(event.target.value);
-    };
+  // Handle the change of the dropdown value for category
+  const handleCategoryChange = (event) => {
+    setFilterByCategory(event.target.value);
+  };
 
-    // Handle the change of the dropdown value for category
-    const handleCategoryChange = (event) => {
-        setFilterByCategory(event.target.value);
-    };
+  // Handle the change of the dropdown value for brandName
+  const handleBrandChange = (event) => {
+    setSortByBrand(event.target.value);
+  };
 
-    // Handle the change of the dropdown value for brandName
-    const handleBrandChange = (event) => {
-        setSortByBrand(event.target.value);
-    };
+  const handleNewProduct = () => {
+    navigate("/add-product");
+  };
 
-    return (
-        <div>
-            <h1>Product list</h1>
-            <div>
-                <label>Filter by Inventory:</label>
-                <select value={filterByInventory} onChange={handleInventoryChange}>
-                    <option value="">All</option>
-                    <option value="Internal Use">Internal Use</option>
-                    <option value="Retail">Retail</option>
-                </select>
-            </div>
-            <div>
-                <label>Filter by Category:</label>
-                <select value={filterByCategory} onChange={handleCategoryChange}>
-                    <option value="">All</option>
-                    <option value="Hair Care">Hair Care</option>
-                    <option value="Skin Care">Skin Care</option>
-                    <option value="Body Care">Body Care</option>
-                    <option value="Make Up">Make Up</option>
-                </select>
-            </div>
-            <div>
-                <label>Sort by Brand Name:</label>
-                <select value={sortByBrand} onChange={handleBrandChange}>
-                    <option value="">None</option>
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
-                </select>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        {renderTableHeader()}
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderTableData()}
-                </tbody>
-            </table>
-        </div>
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`; // Formats the date to display only date portion
+  };
+
+  const calculateStatus = (inventoryId) => {
+    const notification = notificationData.find(
+      (notification) => notification.inventoryId === inventoryId
     );
-};
 
+    const lowStockThreshold = notification.lowStockThreshold;
+    const inventory = inventoryData.find(
+      (inventory) => inventory._id === inventoryId
+    );
+
+    const currentDate = new Date();
+    const expiryDate = new Date(inventory.expiryDate);
+
+    if (expiryDate < currentDate) return "Expired";
+    if (inventory.stockQuantity <= 0) return "Out of Stock";
+    if (inventory.stockQuantity <= lowStockThreshold) return "Low Stock";
+    return "In Stock";
+  };
+
+  return (
+    <div>
+      <h1>Product list</h1>
+      <div>
+        <div>
+          <button onClick={handleNewProduct}>Register New Product</button>
+        </div>
+        <div>
+          <label>Filter by Inventory:</label>
+          <select
+            className="dropdown"
+            value={filterByInventory}
+            onChange={handleInventoryChange}
+          >
+            <option value="">All</option>
+            {inventoryTypeData.map((type) => (
+              <option key={type.value} value={type.value}>
+                {type.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Filter by Category:</label>
+          <select
+            className="dropdown"
+            value={filterByCategory}
+            onChange={handleCategoryChange}
+          >
+            <option value="">All</option>
+            {productCategoryData.map((type) => (
+              <option key={type.value} value={type.value}>
+                {type.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Sort by Brand Name:</label>
+          <select value={sortByBrand} onChange={handleBrandChange}>
+            <option value="">None</option>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>{renderTableHeader()}</tr>
+        </thead>
+        <tbody>{renderTableData()}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default ProductList;
--- src/components/StaffCheckOutModal.js
+++ src/components/StaffCheckOutModal.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import notifyPeriodData from "./predefined_data/notifyperiod.json";
 import checkoutReasonData from "./predefined_data/checkoutreason.json";
-import axios from "axios"
+import axios from "axios";
 
-const StaffCheckOutModal = ({ handleClose, productData, inventoryId, handleReloadInternalData }) => {
+const StaffCheckOutModal = ({
+  handleClose,
+  productData,
+  inventoryId,
+  stockQuantity,
+  handleReloadInternalData,
+}) => {
   const [quantity, setQuantity] = useState(1); // Default quantity is 1
   const [reason, setReason] = useState(""); // State for reason dropdown
   const [dateOfOpen, setDateOfOpen] = useState(""); // State for date of open
@@ -24,14 +30,58 @@ const StaffCheckOutModal = ({ handleClose, productData, inventoryId, handleReloa
     }
   }, []);
 
+  const validateForm = () => {
+    // Validate quantity
+    if (quantity <= 0 || quantity > stockQuantity) {
+      alert("Please enter a valid quantity within the available stock.");
+      return false;
+    }
+
+    // Validate reason
+    if (!reason) {
+      alert("Please select a reason for checkout.");
+      return false;
+    }
+
+    // Validate dateOfOpen
+    if (!dateOfOpen) {
+      alert("Please select a date of open.");
+      return false;
+    }
+
+    const today = new Date();
+    const [year, month, day] = dateOfOpen.split("-").map(Number);
+    const selectedDate = new Date(year, month - 1, day);
+
+    // Set the time part of today's date to 00:00:00 to compare date only
+    today.setHours(0, 0, 0, 0);
+    selectedDate.setHours(0, 0, 0, 0);
+
+
+    if (selectedDate < today) {
+      alert("Date of open cannot be earlier than today's date.");
+      return false;
+    }   
+
+    // Validate expirationReminder and notifyPeriod
+    if (expirationReminder && !notifyPeriod) {
+      alert("Please select a notify period for expiration reminder.");
+      return false;
+    }
+
+    return true; // All validations passed
+  };
+
   const handleProceed = () => {
- 
+    if (!validateForm()) {
+      return;
+    }
+
     const periodAfterOpening = productData.periodAfterOpening;
     const [year, month, day] = dateOfOpen.split("-").map(Number);
     const afterOpenDate = new Date(year, month - 1, day);
     afterOpenDate.setMonth(afterOpenDate.getMonth() + periodAfterOpening); // Add the periodAfterOpening to the month
     const useByDate = afterOpenDate.toISOString();
-
 
     const formData = {
       userId: "user_id",
@@ -44,28 +94,31 @@ const StaffCheckOutModal = ({ handleClose, productData, inventoryId, handleReloa
       checkoutDate: new Date().toISOString(),
       openingDate: dateOfOpen,
       reminderTime: notifyPeriod,
-      isExpirationReminder: expirationReminder
+      isExpirationReminder: expirationReminder,
     };
 
     //POST request to the API
-    axios.post("https://api.lumiereapp.ca/api/v1/checkout", formData)
-      .then(response => {
+    axios
+      .post("https://api.lumiereapp.ca/api/v1/checkout", formData)
+      .then((response) => {
         alert("Checkout successful!");
         handleReloadInternalData(response.data);
         handleClose();
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle error
         console.error("Error during checkout:", error);
         alert("Error during checkout. Please try again.");
         handleClose();
-      });        
+      });
   };
 
   return (
     <div className="overlay">
       <div className="overlay-content">
-        <button className="close-button" onClick={handleClose}>x</button>
+        <button className="close-button" onClick={handleClose}>
+          x
+        </button>
         <h2>Staff Check Out</h2>
         <label htmlFor="quantity">Quantity:</label>
         <input
@@ -73,6 +126,7 @@ const StaffCheckOutModal = ({ handleClose, productData, inventoryId, handleReloa
           name="quantity"
           value={quantity}
           min="1"
+          max={stockQuantity}
           onChange={(e) => setQuantity(e.target.value)}
         />
         <label htmlFor="reason">Reason:</label>

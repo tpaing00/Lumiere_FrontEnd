dae45ba6fbfbd571da61877d253b55d2809bac0f
greeshma017 <gdharmapalan00@mylangara.ca>
2024-03-08 23:51:29 -0800

    feat: P2S-247 update on NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,135 +1,116 @@
-import React from 'react';
-import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material/';
-import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
-import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
-import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
-import CropFreeOutlinedIcon from '@mui/icons-material/CropFreeOutlined';
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  useMediaQuery,
+} from "@mui/material/";
+import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+import InsertChartOutlinedIcon from "@mui/icons-material/InsertChartOutlined";
+import CropFreeOutlinedIcon from "@mui/icons-material/CropFreeOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+
+  const drawerContent = (
+    <List>
+      <ListItem component={Link} to="/">
+        <ListItemButton>
+          <ListItemIcon>
+            <InboxOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText secondary="Dashboard" />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem component={Link} to="/inventory">
+        <ListItemButton>
+          <ListItemIcon>
+            <Inventory2OutlinedIcon />
+          </ListItemIcon>
+          <ListItemText secondary="Inventory" />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem component={Link} to="/analytics">
+        <ListItemButton>
+          <ListItemIcon>
+            <InsertChartOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText secondary="Analytics" />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem component={Link} to="/add-product">
+        <ListItemButton>
+          <ListItemIcon>
+            <AddBoxOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText secondary="Register Product" />
+        </ListItemButton>
+      </ListItem>
+
+      <ListItem component={Link} to="/scanner">
+        <ListItemButton>
+          <ListItemIcon>
+            <CropFreeOutlinedIcon />
+          </ListItemIcon>
+          <ListItemText secondary="Quick Scan" />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  );
 
   return (
     <>
-      <Box component='nav'>
-        <List>
-          <ListItem component={Link} to='/'>
-            <ListItemButton>
-              <ListItemIcon>
-                <InboxOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText secondary='Dashboard' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem component={Link} to='/inventory'>
-            <ListItemButton>
-              <ListItemIcon>
-                <Inventory2OutlinedIcon />
-              </ListItemIcon>
-              <ListItemText secondary='Inventory' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem component={Link} to='/analytics'>
-            <ListItemButton>
-              <ListItemIcon>
-                <InsertChartOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText secondary='Analytics' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem component={Link} to='/add-product'>
-            <ListItemButton>
-              <ListItemIcon>
-                <AddBoxOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText secondary='Register Product' />
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem component={Link} to='/scanner'>
-            <ListItemButton>
-              <ListItemIcon>
-                <CropFreeOutlinedIcon />
-              </ListItemIcon>
-              <ListItemText secondary='Quick Scan' />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Box>
+      {isMobile ? (
+        <Box>
+          <Box position="absolute" top={10} left={10}>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerToggle}
+              edge="start"
+            >
+              {open ? <CloseIcon onClick={handleDrawerToggle} /> : <MenuIcon />}
+            </IconButton>
+          </Box>
+          <Drawer
+            anchor="left"
+            open={open}
+            // onClose={() => setOpen(false)}
+            variant="persistent"
+            PaperProps={{
+              sx: {
+                marginTop: "55px", // Set margin top to start below the menu icon
+                width: "100%", // Set width to 100% for full width
+              },
+            }}
+          >
+            {drawerContent}
+          </Drawer>
+        </Box>
+      ) : (
+        <Box component="nav">{drawerContent}</Box>
+      )}
     </>
-    //     <nav>
-    //     <ul>
-    //         <li><Link to="/">Home</Link></li>
-    //         <li><Link to="/inventory">Inventory</Link></li>
-    //         <li><Link to="/add-product">Register product</Link></li>
-    //         <li><Link to="/scanner">Quick Scan</Link></li>
-    //     </ul>
-    // </nav>  
   );
-
-  // return (
-  //   <>
-  //     <Box component='nav'>
-  //       <List>
-  //         <ListItem component='a' href='http://lumiereapp.ca/'>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <InboxOutlinedIcon />
-  //             </ListItemIcon>
-  //             <ListItemText secondary='Home' />
-  //           </ListItemButton>
-  //         </ListItem>
-
-  //         <ListItem component='a' href='http://lumiereapp.ca/inventory'>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <Inventory2OutlinedIcon />
-  //             </ListItemIcon>
-  //             <ListItemText secondary='Inventory' />
-  //           </ListItemButton>
-  //         </ListItem>
-
-  //         <ListItem component='a' href='http://lumiereapp.ca/analytics'>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <InsertChartOutlinedIcon />
-  //             </ListItemIcon>
-  //             <ListItemText secondary='Analytics' />
-  //           </ListItemButton>
-  //         </ListItem>
-
-  //         <ListItem component='a' href='http://lumiereapp.ca/add-product'>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <AddBoxOutlinedIcon />
-  //             </ListItemIcon>
-  //             <ListItemText secondary='Register Product' />
-  //           </ListItemButton>
-  //         </ListItem>
-
-  //         <ListItem component='a' href='http://lumiereapp.ca/scanner'>
-  //           <ListItemButton>
-  //             <ListItemIcon>
-  //               <CropFreeOutlinedIcon />
-  //             </ListItemIcon>
-  //             <ListItemText secondary='Quick Scan' />
-  //           </ListItemButton>
-  //         </ListItem>
-  //       </List>
-  //     </Box>
-  //   </>
-  //   //     <nav>
-  //   //     <ul>
-  //   //         <li><Link to="/">Home</Link></li>
-  //   //         <li><Link to="/inventory">Inventory</Link></li>
-  //   //         <li><Link to="/add-product">Register product</Link></li>
-  //   //         <li><Link to="/scanner">Quick Scan</Link></li>
-  //   //     </ul>
-  //   // </nav>  
-  // );
 };
 
 export default NavBar;

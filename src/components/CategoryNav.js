4c17b4dcf7e5793e2d9edf3b0ab5a3aeb3c35c67
greeshma017 <gdharmapalan00@mylangara.ca>
2024-03-23 04:35:11 -0700

    style:P2S-288 update on categoryNav

--- src/components/CategoryNav.js
+++ src/components/CategoryNav.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Typography, Grid, IconButton, useTheme } from "@mui/material";
 import { SvgIcon } from "@mui/material";
-
-import All from '../assets/icons/All.svg';
-import HairCare from '../assets/icons/HairCare.svg';
-import SkinCare from '../assets/icons/Face-Icon.svg';
-import BodyCare from '../assets/icons/BodyCare.svg';
-import MakeUp from '../assets/icons/Make-Up-Icon.svg';
+import All from "../assets/icons/All.svg";
+import HairCare from "../assets/icons/HairCare.svg";
+import SkinCare from "../assets/icons/Face-Icon.svg";
+import BodyCare from "../assets/icons/BodyCare.svg";
+import MakeUp from "../assets/icons/Make-Up-Icon.svg";
 
 const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
   const theme = useTheme();
@@ -19,22 +18,30 @@ const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
         alignItems="center"
         style={{ marginTop: "-50px", marginLeft: "auto", marginRight: "auto" }}
       >
-        <Grid item container xs={2} direction="column"  alignItems="center" justifyContent="center">
-        <Grid item>
+        <Grid item container xs={1} direction="column" alignItems="center">
+          <Grid item>
             <IconButton
               onClick={() => handleCategoryChange("All")}
-              color={selectedCategory === "All" ? "primary" : "black"}
+              color={selectedCategory === "All" ? "primary" : "default"}
               style={{
-                marginTop: '10px',
-                background: selectedCategory === "All" ? '#003c5c' : 'white',
-                boxShadow:'0px 2px 4px rgba(0,0,0,0.1)'
+                marginTop: "10px",
+                background:
+                  selectedCategory === "All" ? "#003c5c" : "white",
+                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
               }}
             >
-              <SvgIcon component={All} style={{ fill: selectedCategory === "All" ? theme.palette.primary.contrastText : theme.palette.text.body }} />
+              <SvgIcon component={All} viewBox="0 0 24 24" />
             </IconButton>
           </Grid>
           <Grid item style={{ marginTop: "auto" }}>
-            <Typography variant="body2" align="center" style={{ fontWeight: selectedCategory === "All" ? 'bold' : 'normal' }}>
+            <Typography
+              variant="body2"
+              align="center"
+              style={{
+                fontWeight:
+                  selectedCategory === "All" ? "bold" : "normal",
+              }}
+            >
               All Categories
             </Typography>
           </Grid>
@@ -45,17 +52,24 @@ const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
               onClick={() => handleCategoryChange("Hair Care")}
               color={selectedCategory === "Hair Care" ? "primary" : "default"}
               style={{
-                marginTop: '10px',
-                background:selectedCategory === "Hair Care" ? "#003c5c" : "white",
-                boxShadow:'0px 2px 4px rgba(0,0,0,0.1)'
-                
+                marginTop: "10px",
+                background:
+                  selectedCategory === "Hair Care" ? "#003c5c" : "white",
+                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
               }}
             >
               <SvgIcon component={HairCare} viewBox="0 0 24 24" />
             </IconButton>
           </Grid>
           <Grid item style={{ marginTop: "auto" }}>
-            <Typography variant="body2" align="center">
+            <Typography
+              variant="body2"
+              align="center"
+              style={{
+                fontWeight:
+                  selectedCategory === "Hair Care" ? "bold" : "normal",
+              }}
+            >
               Hair Care
             </Typography>
           </Grid>
@@ -66,17 +80,24 @@ const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
               onClick={() => handleCategoryChange("Skin Care")}
               color={selectedCategory === "Skin Care" ? "primary" : "default"}
               style={{
-                marginTop: '10px',
-                background:selectedCategory === "Skin Care" ? "#003c5c" : "white",
-                boxShadow:'0px 2px 4px rgba(0,0,0,0.1)'
-                
+                marginTop: "10px",
+                background:
+                  selectedCategory === "Skin Care" ? "#003c5c" : "white",
+                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
               }}
             >
               <SvgIcon component={SkinCare} />
             </IconButton>
           </Grid>
           <Grid item style={{ marginTop: "auto" }}>
-            <Typography variant="body2" align="center">
+            <Typography
+              variant="body2"
+              align="center"
+              style={{
+                fontWeight:
+                  selectedCategory === "Skin Care" ? "bold" : "normal",
+              }}
+            >
               Skin Care
             </Typography>
           </Grid>
@@ -85,20 +106,25 @@ const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
           <Grid item>
             <IconButton
               onClick={() => handleCategoryChange("Make Up")}
-              color={selectedCategory === "Makeup" ? "primary" : "default"}
+              color={selectedCategory === "Make Up" ? "primary" : "default"}
               style={{
-                marginTop: '10px',
-                background:selectedCategory === "Makeup" ? "#003c5c" : "white",
-                boxShadow:'0px 2px 4px rgba(0,0,0,0.1)'
-                
+                marginTop: "10px",
+                background: selectedCategory === "Make Up" ? "#003c5c" : "white",
+                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
               }}
             >
               <SvgIcon component={MakeUp} />
             </IconButton>
           </Grid>
           <Grid item style={{ marginTop: "auto" }}>
-            <Typography variant="body2" align="center">
-              Makeup
+            <Typography
+              variant="body2"
+              align="center"
+              style={{
+                fontWeight: selectedCategory === "Make Up" ? "bold" : "normal",
+              }}
+            >
+              Make-up
             </Typography>
           </Grid>
         </Grid>
@@ -108,17 +134,23 @@ const CategoryNav = ({ handleCategoryChange, selectedCategory }) => {
               onClick={() => handleCategoryChange("Body Care")}
               color={selectedCategory === "Body Care" ? "primary" : "default"}
               style={{
-                marginTop: '10px',
-                background:selectedCategory === "Body Care" ? "#003c5c" : "white",
-                boxShadow:'0px 2px 4px rgba(0,0,0,0.1)'
-                
+                marginTop: "10px",
+                background:
+                  selectedCategory === "Body Care" ? "#003c5c" : "white",
+                boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
               }}
             >
               <SvgIcon component={BodyCare} />
             </IconButton>
           </Grid>
           <Grid item style={{ marginTop: "auto" }}>
-            <Typography variant="body2" align="center">
+            <Typography
+              variant="body2"
+              align="center"
+              style={{
+                fontWeight: selectedCategory === "Body Care" ? "bold" : "normal",
+              }}
+            >
               Body Care
             </Typography>
           </Grid>

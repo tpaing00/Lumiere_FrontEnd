a8bf2897392860fe9dddbfddbff67554139ff994
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-04-06 00:02:33 -0700

    fix height/width nav icons and top trend products

--- src/components/CategoryNavIcon.js
+++ src/components/CategoryNavIcon.js
@@ -28,6 +28,7 @@ export default function CategoryNavIcon({
           display: "flex",
           flexWrap: "wrap",
           justifyContent: "center",
+          alignItems: "space-between",
           flexShrink: 0,
         }}
       >
@@ -38,13 +39,14 @@ export default function CategoryNavIcon({
                     background:
                         backgroundRule,
                     boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
-                    display: "flex", justifyContent: "center", alignItems: "center",
+                    display: "flex", justifyContent: "space-between", alignItems: "space-between",
                     flexBasis: "auto",
                     height: { xs: "36px", lg: "57px" },
                     flexGrow: 0,
+                    padding: "12px"
                 }}
             >
-                <SvgIcon component={svgComponent} viewBox={svgViewBox} sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: svgHeight, width: svgWidth, alignSelf: 'center', flexGrow: 0, flexBasis: "auto", }} />
+                <Box component={svgComponent} viewBox={svgViewBox} sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: svgHeight, width: svgWidth, alignSelf: 'center', flexGrow: 0, flexBasis: "auto", }} />
             </IconButton>
             <Typography
                 variant="body2"
@@ -53,7 +55,8 @@ export default function CategoryNavIcon({
                     fontWeight: fonWeightRule,
                     fontSize: { xs: "10px", lg: "14px" },
                     marginTop: "8px",
-                    width: "100%"
+                    width: "100%",
+                    verticalAlign: "text-bottom"
                 }}
             >
                 {bottomText}
